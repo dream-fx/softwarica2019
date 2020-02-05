356c36cedbff34544c39ef6475aba3da34130602
Nischal Shakya <nischal.shk@gmail.com>
2020-02-05 19:50:42 +0545

    hoem post and get added

--- routes/home.js
+++ routes/home.js
@@ -2,17 +2,31 @@ const express = require("express");
 const Home = require("../model/home");
 const router = express.Router();
 
-router.route("/").post((req, res, next) => {
-  let home = new Home(req.body);
-  home.author = req.host._id;
-  console.log(home);
-  home
-    .save()
-    .then(home => {
-      res.statusCode = 201;
-      res.json(home);
-    })
-    .catch(next);
-});
+router
+  .route("/")
+
+  .post((req, res, next) => {
+    let home = new Home(req.body);
+    home.author = req.host._id;
+    console.log(home);
+    home
+      .save()
+      .then(home => {
+        res.statusCode = 201;
+        res.json(home);
+      })
+      .catch(next);
+  })
+  .get((req, res, next) => {
+    Home.find({ author: req.host._id })
+      .then(homes => {
+        console.log(homes);
+        res.json(homes);
+      })
+      .catch(err => {
+        next(err);
+      });
+  });
+
 
 module.exports = router;
