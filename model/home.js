c9fc4e063425c6ef43bab5efe561f68e7672a25e
Nischal Shakya <nischal.shk@gmail.com>
2020-02-05 19:36:34 +0545

    Home schema done

---
+++ model/home.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const homeSchema = new mongoose.Schema({
+  homeName: {
+    type: String,
+    required: true
+  },
+  homeDetail: {
+    type: String,
+    required: true
+  },
+  homeCategory: {
+    type: String,
+    required: true
+  },
+  homeImage: {
+    type: String
+  },
+  homePrice: {
+    type: Number
+  },
+  homeLocation: {
+    type: String
+  },
+  homePerson: {
+    type: Number
+  },
+  homeLat: {
+    type: Number
+  },
+  homeLong: {
+    type: Number
+  },
+  homeAvail: {
+    type: Boolean
+  },
+
+  homeCreatedAt: {
+    type: Date
+  },
+  
+  hostName:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Host"
+}
+
+});
+
+module.exports = mongoose.model("Home", homeSchema);
