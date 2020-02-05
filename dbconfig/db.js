75dbbe390d72829cac21932736a79275c44c36fe
Nischal Shakya <nischal.shk@gmail.com>
2020-02-05 18:00:45 +0545

    Connection Success

--- dbconfig/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const databaseName = 'NischalShakyaBackend';
-mongoose.connect('mongodb://127.0.0.1:27017/' + databaseName, 
-    {
-        useNewUrlParser: true,
-        useCreateIndex: true
-    })
--- index.js
+++ index.js
@@ -1,54 +1,54 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const cors = require('cors');
-const multer = require('multer')
+const express = require("express");
+//const bodyParser = require("body-parser");
+//const path = require("path");
+const cors = require("cors");
+//const multer = require("multer");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
+const dotenv = require("dotenv").config();
 
+//require("./dbconfig/db");
+const Guest = require("./routes/guest");
+const Host = require("./routes/host");
+const Home = require("./routes/home");
+const Booking = require("./routes/booking");
+const auth = require("./middleware/auth");
+const Upload = require("./routes/upload");
 
 const app = express();
-//const jwt = require('jsonwebtoken');
-app.options('*', cors());
-app.use(morgan('tiny'));
+app.options("*", cors());
+app.use(morgan("tiny"));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: true }));
 
+mongoose
+  .connect(process.env.URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
+  .then(
+    db => {
+      console.log("Successfully connected to MongodB server");
+    },
+    err => console.log(err)
+  );
 
+app.use("/guest", Guest);
+app.use("/upload", Upload);
+app.use(auth.verifyGuest);
+// app.use("/host", Host);
+// app.use("/home", Home);
+// app.use("/booking", Booking);
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.statusCode = 500;
+  res.json({ status: err.message });
+});
 
-
-require('./dbconfig/db');
-const Guest = require('./model/guest');
-const Host = require('./model/host');
-const Home = require('./model/home');
-const Booking = require('./model/booking');
-//const auth = require('./middleware/auth');
-const Upload = require('./routes/upload');
-
-mongoose.connect(process.env.URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-    .then((db) => {
-        console.log("Successfully connected to MongodB server");
-    }, (err) => console.log(err));
-
-
-    app.use('/guest', Guest);
-    app.use('/upload', Upload);
-    app.use(auth.verifyUser);
-    app.use('/host', Host);
-    app.use('/home', Home);
-    app.use('/booking', Booking);
-    app.use('/wishlist', Wishlist);
-
-    app.use((err, req, res, next) => {
-        console.error(err.stack);
-        res.statusCode = 500;
-        res.json({ status: err.message });
-    });
-    
-    app.listen(process.env.PORT, () => {
-        console.log(`App is running at localhost:${process.env.PORT}`);
-    });
-
-
-
-
+app.listen(process.env.PORT, () => {
+  console.log(`App is running at localhost:${process.env.PORT}`);
+});
